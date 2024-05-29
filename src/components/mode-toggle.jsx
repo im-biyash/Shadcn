b72012e25473b7fa9b3029e37d85e1ceeb4d8226
biyash <bsstha21@gmail.com>
2024-05-29 21:03:07 +0545

    cahnge

--- src/components/mode-toggle.jsx
+++ src/components/mode-toggle.jsx
@@ -23,7 +23,7 @@ export function ModeToggle() {
       ) : (
         <Moon className="h-[1.2rem] w-[1.2rem] transition-all" />
       )}
-      <span className="sr-only">Toggle theme</span>
+      <span className="sr-only">Toggle themes</span>
     </Button>
   );
 }
